756a4897d8f88b043228840bdf9e366877ac6fe2
Sanika Chandode <sanu30chandode@gmail.com>
2023-01-13 14:51:49 +0530

    required Documents page

---
+++ requireddocuments/android/app/src/main/kotlin/com/example/requireddocuments/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.requireddocuments
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ requireddocuments/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'text_title.dart';
+
+void main() {
+  runApp(
+    HomePage(),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Required Documents'),
+          ),
+          body: Container(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: <Widget>[
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Cap allotment letter'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'College fee receipt'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Marksheet of SSC(10th std)'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Marksheet of HSC(12th std)'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Previous Year Marksheet'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'All semester Marksheets'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Caste Certificate'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(
+                    title:
+                        'Income Certificate of current financial year(from Tehsildar)'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Bank Passbook'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Self Declaration'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Domicile Certificate'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Rashan Card'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Hostel Certificate(if Required)'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Gap Certificate(if Required)'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Texttitle(title: 'Handicap Certificate(if Required)'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Texttitle extends StatelessWidget {
+  Texttitle({required this.title});
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 20.0,
+      ),
+    );
+  }
+}
